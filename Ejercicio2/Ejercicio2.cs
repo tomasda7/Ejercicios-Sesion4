ba0b5d6f3a866157d048c926acf288039232943d
tomasda7 <tomasda7@outlook.com>
2023-02-08 15:02:32 -0300

    Agrego ejercicios 2 y 3

---
+++ Ejercicio2/Ejercicio2.cs
@@ -0,0 +1,20 @@
+﻿//Ejercicio 2 - Do while
+//Escribe un programa que realice estos pasos:
+//Reciba al menos un número por consola
+//Determine si el número es positivo o negativo
+//Presente un contador para cada tipo (positivo y negativo).
+
+Console.WriteLine("Ingresa un numero positivo o negativo: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num > 0) {
+    Console.WriteLine("Contador de numeros positivos: ");
+    int i = 1;
+    do { Console.WriteLine(i); i++; } while (i <= num);
+
+} else if(num < 0) {
+    Console.WriteLine("Contador de numeros negativos: ");
+    int j = -1;
+    do { Console.WriteLine(j); j--; } while (j >= num);
+
+} else { Console.WriteLine("El numero 0 no es positivo ni negativo."); }
---
+++ Ejercicio3/Ejercicio3.cs
@@ -0,0 +1,65 @@
+﻿// Ejercicio 3 - For
+//Escribe un programa que realice estos pasos:
+//Reciba 3 datos:
+//ancho
+//alto
+//relleno o no
+//Dibuje en consola con un mismo caracter, por ejemplo *, un rectángulo de las dimensiones introducidas y
+//use el tercer dato para discernir si el rectángulo está relleno (tiene más * dentro) o no.
+//En caso de recibir el mismo número n dos veces debe dibujar un cuadrado de lado n.
+
+void DrawRectangle(int ancho, int alto, bool relleno) {
+
+    if(relleno == true) {
+        //anchura
+        for (int i = 0; i < ancho; i++)
+        {
+            Console.Write("* ");
+
+        }
+
+        Console.WriteLine("");
+    
+        //altura
+        for (int j = 0; j < alto; j++)
+        {
+            Console.WriteLine(" ****************** ");
+        }
+
+        //anchura
+        for (int i = 0; i < ancho; i++)
+        {
+            Console.Write("* ");
+
+        }
+        
+    } else
+    {
+        //anchura
+        for (int i = 0; i < ancho; i++)
+        {
+            Console.Write("* ");
+
+        }
+
+        //altura
+        for (int j = 0; j < alto; j++)
+        {
+	          Console.WriteLine(" ");
+        }
+
+        //anchura
+        for (int i = 0; i < ancho; i++)
+        {
+            Console.Write("* ");
+
+        }
+        
+    }
+
+}
+
+DrawRectangle(10,5,true);
+
+
+
