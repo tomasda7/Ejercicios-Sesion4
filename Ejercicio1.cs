ea0f4689098e645258ffdbc44d8d9fe0b835424f
tomasda7 <tomasda7@outlook.com>
2023-02-08 14:57:44 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio1.cs
@@ -0,0 +1,14 @@
+﻿//Ejercicio 1 - While
+//Escribe una tabla de multiplicar del 1 al 10 para un número entero que recibe por consola.
+//Es decir, un programa que presente para el 1:
+
+Console.WriteLine("Ingrese un numero entero: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int i = 1;
+
+Console.WriteLine($"Tabla de mulplicar del {num}: ");
+while(i <= 10) {
+    Console.WriteLine($"{num} * {i} = " + num * i);
+    i++;
+}
